e54ce8c617bdc76bd17e296655dcf54564158bae
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-11 09:49:48 -0500

    Complete basic05

--- basic05ReturnLargestNumbersInArrays.js
+++ basic05ReturnLargestNumbersInArrays.js
@@ -1,53 +1,13 @@
 function largestOfFour(arr) {
-    let longestNum = 0;
-    let newArr = []
-    for (let subArr of arr) {
-        for (let num of subArr) {
-            let longest = subArr[0];
-            if (num >= longest) {
-                longest = num;
-            }
-            if (longest > longestNum) {
-                longestNum = longest;
-            }
-        }
-        console.log(longestNum);
-        newArr.push(longestNum);
+    const arrLength = arr.length;
+    for (i = 0; i < arr.length; i++) {
+        let arrSort = arr[[i]];
+        let arrSortLength = arrSort.length;
+        arrSort.sort(function(a, b){return a-b});
+        let arrSortLargest = arrSort[arrSortLength - 1];
+        arr[i] = arrSortLargest;
     }
-    console.log(newArr);
-    /*
-    arr.splice(0,4);
-    console.log(arr);
-    arr.push(newArr);
-    console.log(arr);
-    */
-    return newArr;
-}
-  
-  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
-  
-  
-  
-  /* 
-  function findLongestWordLength(words) {
-    let wordsArray = words.split(' ');
-    let longestWord = wordsArray[0].length;
-  
-    for (const word of wordsArray) {
-      if (word.length > longestWord) {
-        longestWord = word.length;
-      }
-    }
-    return longestWord;
+    return(arr);
   }
   
-  Start with 4 arrays
-  For array[0]
-      longest = num[0]
-      if num > longest
-        longest = num
-  
-  Return an array consisting of the largest number from each provided sub-array. 
-  For simplicity, the provided array will contain exactly 4 sub-arrays.
-  Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
-  */
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
