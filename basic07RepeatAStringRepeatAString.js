c6200b66e7f536ebc17fe15238d5e80d429ca927
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-11 13:00:58 -0500

    Complete basic07

---
+++ basic07RepeatAStringRepeatAString.js
@@ -0,0 +1,21 @@
+function repeatStringNumTimes(str, num) {
+    if (num > 0) {
+        let n = str;
+        for (i = 0; i < num-1; i++) {
+            n = n.concat(str);
+        }
+        str = n;
+    }
+    else {
+        return false;
+    }
+    console.log(str);
+    return str;
+  }
+  
+  repeatStringNumTimes("abc", 3);
+
+  /* 
+  Repeat a given string str (first argument) for num times (second argument). 
+  Return an empty string if num is not a positive number.
+  */
